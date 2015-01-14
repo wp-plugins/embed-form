edae73f64916bc54e45a3fd5cf897d2147b28894
paul_simpson <paul_simpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-14 19:12:34 +0000

    Updates for SSL and form picker
    git-svn-id: https://plugins.svn.wordpress.org/embed-form/trunk@1068057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jotform-wp-embed.js
+++ jotform-wp-embed.js
@@ -19,7 +19,7 @@
                     function openWizard() {
                         window.jotformFormPicker.openWizard(function(formID){
                             if (formID !== undefined && formID.length > 0 && formID !== "Please enter your form id") {
-                                ed.execCommand('mceInsertContent', false, '[jotform id="'+formID+'"]');
+                                tinyMCE.activeEditor.execCommand('mceInsertContent', 0, '[jotform id="' + formID + '"]');
                             }
                         });
                     }
@@ -27,7 +27,7 @@
                         openWizard();
                     }
                     else {
-                        jQuery.getScript("http://js.jotform.com/JotFormFormPicker.js", function(data, textStatus, jqxhr) {
+                        jQuery.getScript("//js.jotform.com/JotFormFormPicker.js", function(data, textStatus, jqxhr) {
                             event.preventDefault();
                             window.jotformFormPicker = new JotFormFormPicker();
                             openWizard();
